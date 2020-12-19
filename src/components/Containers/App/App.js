c1dd18088e69aedb813a2dbbdfa29fb1b868544d
Tomer Raitz <tomeraitz1@gmail.com>
2020-12-19 19:01:08 +0200

    fixed service

--- src/components/Containers/App/App.js
+++ src/components/Containers/App/App.js
@@ -8,28 +8,32 @@ import httpRequest from '../../../services/httpRequest.service';
 class  App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isTesterList : false, testerName : '', serverResponse : {}};
+    this.state = { isTesterListOn : false, testerName : '', serverResponse : {}};
  }
 
  setData = (data) =>{
-  this.setState({isTesterList : true,serverResponse : data})
+  this.setState({isTesterListOn : true,serverResponse : data})
  }
 
- showTable = (testerName) =>{
+ showTable =async (testerName) =>{
    if(!this.state.testerName){
     this.setState({testerName : testerName});
-    httpRequest(testerName,this.setData)
+    const res = await httpRequest(testerName).catch(e=>{
+      console.error(e)
+      this.setState({error : e})
+    })
+    res.data ? this.setData({data : res.data}) : this.setData({error : 'No data found'})
    }
  }
 
- goToHome = ()=>this.setState({ isTesterList : false, testerName : '', serverResponse : {}})
+ goToHome = ()=>this.setState({ isTesterListOn : false, testerName : '', serverResponse : {}})
  
   render(){
-    const {isTesterList , testerName, serverResponse} = this.state;
+    const {isTesterListOn , testerName, serverResponse} = this.state;
     return (
       <div className="App">
         <BrowserRouter>
-            {isTesterList ? 
+            {isTesterListOn ? 
             <Redirect exact from="/" to={`/testersList/${testerName}`} /> :
             !testerName && <Redirect exact push to='/' />}
           <Switch>
@@ -40,7 +44,7 @@ class  App extends React.Component {
               />
             <Route 
                 path="/testersList/:id"
-                render={() => (isTesterList ? <TestersList goToHome={this.goToHome}  serverResponse={serverResponse} />: <SearchTester />)}
+                render={() => (isTesterListOn ? <TestersList goToHome={this.goToHome}  serverResponse={serverResponse} />: <SearchTester />)}
                 exact 
               />
           </Switch>
--- src/services/httpRequest.service.js
+++ src/services/httpRequest.service.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
-// const URL  = "https://test-api.techsee.me/api/ex/";
 
-
-export default function getTesters(testerName, callback){
-  axios.get(`http://localhost:8000/getTesters/${testerName}`)
-  .then(response => response.data ? callback({data :response.data}) : callback({error : 'No data found'}))
-  .catch(e=>{
-    callback({error : e});
-  })
+export default async  function getTesters(testerName){
+ return await axios.get(`http://localhost:8000/getTesters/${testerName}`);
 }
 
